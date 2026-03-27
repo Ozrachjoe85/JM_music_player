47b095f414f108124e374f6c81c57876a99a9dfa
Ozrachjoe85 <ozrachjoe85@gmail.com>
2026-03-27 09:29:56 -0600

    Create Color.kt

---
+++ app/src/main/java/com/jm/musicplayer/ui/theme/Color.kt
@@ -0,0 +1,10 @@
+package com.jm.musicplayer.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val VoidBlack = Color(0xFF000000)
+val VoidCyan = Color(0xFF00FFFF)      // Electric Cyan
+val VoidPurple = Color(0xFF7000FF)    // Deep Intelligent Purple
+val VoidPink = Color(0xFFFF007F)      // Hot Pink for passion/favs
+val VoidGreen = Color(0xFF39FF14)     // Neon Green for success
+val DarkGlass = Color(0x33FFFFFF)     // For Glassmorphism effects
