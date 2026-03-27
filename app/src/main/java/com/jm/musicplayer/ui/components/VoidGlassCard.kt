60a56cb88295c83792fbdf5d27c723e7955c93d8
Ozrachjoe85 <ozrachjoe85@gmail.com>
2026-03-27 09:50:46 -0600

    Update VoidGlassCard.kt

--- app/src/main/java/com/jm/musicplayer/ui/components/VoidGlassCard.kt
+++ app/src/main/java/com/jm/musicplayer/ui/components/VoidGlassCard.kt
@@ -4,11 +4,9 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerCodes
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.blur
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
